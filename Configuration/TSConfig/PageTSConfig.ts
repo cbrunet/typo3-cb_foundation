81bec9560ac75db5cc4459067fdd7997d4f41441
Charles Brunet <charles@cbrunet.net>
2014-07-15 21:58:45 -0400

    Panel frames.

--- Configuration/TSConfig/PageTSConfig.ts
+++ Configuration/TSConfig/PageTSConfig.ts
@@ -29,3 +29,16 @@ mod.wizards.newContentElement.wizardItems.tx_cbfoundation {
     }
     show = orbit, clearing, flexvid, breadcrumbs
 }
+
+TCEFORM {
+    tt_content {
+        section_frame {
+            addItems {
+                100 = LLL:EXT:cb_foundation/Resources/Private/Language/locallang_db.xlf:frame.panel
+                101 = LLL:EXT:cb_foundation/Resources/Private/Language/locallang_db.xlf:frame.panelradius
+                102 = LLL:EXT:cb_foundation/Resources/Private/Language/locallang_db.xlf:frame.callout
+                103 = LLL:EXT:cb_foundation/Resources/Private/Language/locallang_db.xlf:frame.calloutradius
+            }
+        }
+    }
+}
